71d13be984484fd88fc82adb3f85bc53d81ff6f8
Tabish764 <140270791+Tabish764@users.noreply.github.com>
2024-03-05 16:55:45 +0500

    Add files via upload

---
+++ day_3.js
@@ -0,0 +1,13 @@
+"use strict";
+//🚀 Day 3 Challenge: Start Coding! 🚀
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+//Question 8: You should create four lines that look like this:console.log(5 + 3)
+console.log(5 + 3);
+console.log(7 + 1);
+console.log(9 - 1);
+console.log(4 * 2);
+console.log(16 / 2);
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+let favoriteNumber = 7;
+let message = `My favorite number is ${favoriteNumber}`;
+console.log(message);
